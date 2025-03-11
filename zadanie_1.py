24ae22bd4800d90d2dccaba1c6c6628ed778f148
Sylwia-spec <sylwiagradek7@gmail.com>
2025-03-11 19:18:59 +0100

    Dodano zadanie 1

--- zadanie_1.py
+++ zadanie_1.py
@@ -2,16 +2,19 @@
 
 '''
 Importowanie modułu random
+W języku Python przewidziano moduł random generujący liczby pseudolosowe.
 Dokumentacja: https://docs.python.org/3/library/random.html
-import random
 '''
+
+import random
 
 # Tworzenie dwóch list
 lista1 = [5, 12, 50, 111]
 lista2 = ['s', 'y', 'l', 'a']
 
 '''
-Łączenie list za pomocą funkcji zip() - Funkcja zip łączy ze sobą elementy z różnych obiektów iterowalnych, takich jak listy, krotki, zbiory, i zwraca nam iterator. Możemy jej użyć to połączenia ze sobą dwóch list.
+Łączenie list za pomocą funkcji zip() - Funkcja zip łączy ze sobą elementy z różnych obiektów iterowalnych, takich jak listy, krotki, zbiory, i zwraca nam iterator. 
+Możemy jej użyć to połączenia ze sobą dwóch list.
 Dokumentacja: https://docs.python.org/3/library/functions.html#zip
 '''
 zipped = list(zip(lista1, lista2))
@@ -19,8 +22,9 @@ print("Połączone listy:", zipped)
 
 '''
 Wylosowanie liczby z listy - w celu uzyskania losowej liczby niezbędne jest skorzystanie z funkcji "random".
-# Po zaimportowaniu modułu możemy korzystać z wielu funkcji, m.in. losujacych wartość z podanego zakresu, zmieniających w losowy sposób kolejność elementów na liście, wybierających losową wartość spośród podanych.
-# Dokumentacja: https://docs.python.org/3/library/random.html#random.choice
+Po zaimportowaniu modułu możemy korzystać z wielu funkcji, m.in. losujacych wartość z podanego zakresu, zmieniających w losowy sposób kolejność elementów na liście, wybierających losową wartość spośród podanych.
+W naszym przypadku funkcja random wylosowała liczbę ze wskazanej listy.
+Dokumentacja: https://docs.python.org/3/library/random.html#random.choice
 '''
 losowa_liczba = random.choice(lista1)
 print("Losowa liczba:", losowa_liczba)
@@ -29,6 +33,8 @@ print("Losowa liczba:", losowa_liczba)
 Obsługa wyjątku - Błędy zauważone podczas wykonania programu są nazywane wyjątkami (exceptions).
 Większość wyjątków nie jest obsługiwana przez program przez co wyświetlane są informacje o błędzie.
 Możliwe jest pisanie programów, które obsługują wybrane wyjątki.
+ZeroDivisionError to wyjątek w Pythonie, który występuje, gdy próbujemy podzielić liczbę przez zero.
+Aby uniknąć zatrzymania programu, możemy użyć bloku try-except, który przechwyci błąd i pozwoli programowi kontynuować działanie.
 Dokumentacja: https://docs.python.org/3/library/exceptions.html#ZeroDivisionError
 '''
 # Celowe wywołanie ZeroDivisionError
